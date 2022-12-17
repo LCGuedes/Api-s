785f3123c55e031730329045c184de7942e15642
LCGuedes <lucasmainguedes@gmail.com>
2022-12-17 08:26:42 -0300

    feat: ensure SignUpController returns custom error mensage if email or name is not provided

--- src/presentation/controllers/signUp.ts
+++ src/presentation/controllers/signUp.ts
@@ -1,7 +1,9 @@
+import { MissingParamError } from "../errors/missingParamError";
 export class SignUpController {
   handle(httpRequest: any): any {
-    const { email, name } = httpRequest;
-    if (!email) return { statusCode: 400 };
-    if (!name) return { statusCode: 400 };
+    const { email, name } = httpRequest.body;
+    if (!email)
+      return { statusCode: 400, body: new MissingParamError("email") };
+    if (!name) return { statusCode: 400, body: new MissingParamError("name") };
   }
 }
