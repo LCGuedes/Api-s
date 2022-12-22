1cc6987eaab0f8abdfa5e2fba04f570aa8fd3840
LCGuedes <lucasmainguedes@gmail.com>
2022-12-22 09:41:20 -0300

    feat: ensure DbAddAccount calls AddAccountRepository with correct values

--- src/data/protocols/index.ts
+++ src/data/protocols/index.ts
@@ -1,3 +1,4 @@
 export * from "../../domain/useCases/addAccount";
 export * from "../../domain/models/Account";
 export * from "../protocols/encrypter";
+export * from "../protocols/addAccountRepository";
